f733d8fd9ba6e738b2e15b0e070c01998f63ef77
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-26 20:02:59 +0530

    Pattern Problem

---
+++ 18_PatternR.js
@@ -0,0 +1,27 @@
+//         *
+//       *   *
+//     *       *
+//   *           *
+// * * * * * * * * *
+function patternEighteen(n) {
+    let result = "";
+
+    for(let i = 1; i <= n; i++){ // For Row
+        for(let j = 1; j <= n - i; j++) { // For Row Spaces before pattern
+            result += " ";
+        }
+
+        for(let k = 1; k <= (2 * i) - 1; k++) { // For Column
+            if(k == 1 || k == (2 * i) - 1 || i == n){
+                result += "*";
+            }
+            else {
+                result += " ";
+            }
+        }
+        result += '\n';
+    }
+    console.log(result.trimEnd());
+}
+
+patternEighteen(5);
