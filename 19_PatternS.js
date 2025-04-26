5f6fabe6a058840c02669a9191e4145c78cd921f
Muthu kumar <muthukumar24081999@gmail.com>
2025-04-26 22:39:39 +0530

    Pattern Problem

--- 19_PatternS.js
+++ 19_PatternS.js
@@ -1,11 +1,11 @@
 //         * 
-//       * * 
-//     * * * 
-//   * * * * 
-// * * * * * 
-//   * * * * 
-//     * * * 
-//       * * 
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+//   * * * *
+//     * * *
+//       * *
 //         *
 
 function patternNineteen(n) {
@@ -13,6 +13,12 @@ function patternNineteen(n) {
 
     for(let i = (n * 2) - 1; i >= 1 ; i--){
         let rowChange = i > n ? (n * 2) - i : i;
+
+        let spaces = n - rowChange;
+        for(let s = 0; s < spaces; s++){
+            result += "  ";
+        }
+
         for(let j = 1; j <= rowChange; j++){
             result += "* ";
         }
